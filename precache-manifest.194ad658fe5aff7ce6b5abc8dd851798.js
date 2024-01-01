a295a8370af48618f426c41888c6f9c00e2059bd
Viktory94 <124494228+Viktory94@users.noreply.github.com>
2024-01-01 12:32:31 +0300

    Add files via upload

---
+++ precache-manifest.194ad658fe5aff7ce6b5abc8dd851798.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375ed14eb745ee5dc72e12e5766534ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf735d82724b5690e91",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "0bfa8413fdaece8f154b",
+    "url": "/static/js/2.5f90178f.chunk.js"
+  },
+  {
+    "revision": "ccf735d82724b5690e91",
+    "url": "/static/js/main.539a500f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
